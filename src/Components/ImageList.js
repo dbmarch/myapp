c205c7f43445bef1d727cd5b7a6dab1c4495c5ee
dbmarch <dbmarch@gmail.com>
2018-05-30 12:54:54 -0500

    fix the rendering issue

--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -3,62 +3,67 @@ import {Button} from 'mdbreact'
 import ImageItem from './ImageItem';
 
 class ImageList extends Component {
+
     constructor (props) {
         super(props)
+
         this.state = {
             images:  [
-            {imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFvk0e_JcM-SeE6FK3Cz3aOW4ZQod6Rl62a17AlsfcUyVMUd1Q6w",
-             imgName: 'Kiwi'
-            },
-            {imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFvk0e_JcM-SeE6FK3Cz3aOW4ZQod6Rl62a17AlsfcUyVMUd1Q6w",
-             imgName: 'Kiwi2'
-            },
-            {imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFvk0e_JcM-SeE6FK3Cz3aOW4ZQod6Rl62a17AlsfcUyVMUd1Q6w",
-             imgName: 'Kiwi3'
-            },
-        ]
+                {imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFvk0e_JcM-SeE6FK3Cz3aOW4ZQod6Rl62a17AlsfcUyVMUd1Q6w",
+                imgName: 'Kiwi'
+                },
+                {imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFvk0e_JcM-SeE6FK3Cz3aOW4ZQod6Rl62a17AlsfcUyVMUd1Q6w",
+                imgName: 'Kiwi2'
+                },
+                {imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFvk0e_JcM-SeE6FK3Cz3aOW4ZQod6Rl62a17AlsfcUyVMUd1Q6w",
+                imgName: 'Kiwi3'
+                }
+            ]
         };
     }
-
-    imageClicked = (event,id) => {
-        console.log("CLICK:  ID=%d", id);
-        
-    }
+    
+    imageClicked = (event) => {
+        console.log("CLICK: ", event);
+        //event.curentTarget.style.backgroundColor = '#ccc';
+        console.log (event.currentTarget);
+}
     
     render() {
-      return (
-        <div>
-        <div className="container">
-            <h1> Image List </h1>
-            <div className = 'row'>
-                <div className = 'col'>
-                  <ImageItem  image = {this.images[0]} clicked = {this.imageClicked} zoom = {1} />
-                </div>
-                <div className = 'col'>
-                   <ImageItem  image = {this.images[1]} clicked = {this.imageClicked} />
-                </div>
-                <div className = 'col'>
-                   <ImageItem  image = {this.images[2]} clicked = {this.imageClicked} />
-                </div>
-            </div>
-            
-            <div className = 'row'>
-                <div className = 'col'>
-                    <p> Col 1 </p>
-                    <Button outline color="primary">Primary</Button>
 
-                </div>
-                <div className = 'col'>
-                    <p> Col 2 </p>
-                    <Button outline color="primary">Primary</Button>
-                </div>
-                <div className = 'col'>
-                    <p> Col 3 </p>
-                    <Button outline color="primary">Primary</Button>
+      console.log ('render');
+      
+      return (<div>
+                <div className="container">
+                    <h1> Image List </h1>
+                        <div className = 'row'>
+                            <div className = 'col'>
+                            <ImageItem  image = {this.state.images[0]} clicked = {this.imageClicked} key = {0} zoom = {1} />
+                            </div>
+                            <div className = 'col'>
+                            <ImageItem  image = {this.state.images[1]} clicked = {this.imageClicked} key={1}/>
+                            </div>
+                            <div className = 'col'>
+                            <ImageItem  image = {this.state.images[2]} clicked = {this.imageClicked} key={2}/>
+                            </div>
+                        </div> 
+                    
+                        <div className = 'row'>
+                            <div className = 'col'>
+                                <p> Col 1 </p>
+                                <Button outline color="primary">Primary</Button>
+
+                            </div>
+                            <div className = 'col'>
+                                <p> Col 2 </p>
+                                <Button outline color="primary">Primary</Button>
+                            </div>
+                            <div className = 'col'>
+                                <p> Col 3 </p>
+                                <Button outline color="primary">Primary</Button>
+                            </div>
+                        </div>
                 </div>
             </div>
-        </div>
-    </div>
       );
     }
   }
