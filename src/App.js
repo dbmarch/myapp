692e82af1950cef444ab2437459bd028394adda3
dbmarch <dbmarch@gmail.com>
2018-06-13 13:27:50 -0500

    save files - need to clean up the firebase interface.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
+// import firebaseui from 'firebaseui';
 import './App.css';
 import VideoItem from './components/VideoItem';
 import ImageList from './components/ImageList';
 
 
 const VideoSource = "https://media.w3.org/2010/05/sintel/trailer.mp4";
-const FIREBASE_URL = 'https://imagelibrary-47a2f.firebaseio.com/image.json';
+// const FIREBASE_URL = 'https://imagelibrary-47a2f.firebaseio.com/image.json';
+
+
+var config = {
+  apiKey: "AIzaSyA1nmx9Cn7-oibNbYzebngvQRo1uy5aI9w",
+  authDomain: "imagelibrary-47a2f.firebaseapp.com",
+  databaseURL: "https://imagelibrary-47a2f.firebaseio.com",
+  projectId: "imagelibrary-47a2f",
+  storageBucket: "imagelibrary-47a2f.appspot.com",
+  messagingSenderId: "282133875840"
+};
+firebase.initializeApp(config);
+export var storage = firebase.storage();
+
+var storageRef = storage.ref();
+// var gsReference = storage.refFromURL ('gs://imagelibrary-47a2f.appspot.com/cola.jpg');
+storageRef.child('cola.jpg').getDownloadURL().then(function(url) {
+    // `url` is the download URL for 'images/stars.jpg'
+  
+    // This can be downloaded directly:
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'blob';
+    xhr.onload = function(event) {
+      var blob = xhr.response;
+    };
+    xhr.open('GET', url);
+    xhr.send();
+  
+    // Or inserted into an <img> element:
+    var img = document.getElementById('myimg');
+    img.src = url;
+  }).catch(function(error) {
+    // Handle any errors
+  });        
+
 
 class App extends Component {
-  render() {
+    render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to My App</h1>
         </header>s
         <ImageList />
-        <VideoItem videoSource = {VideoSource}/>
+        {/* <VideoItem videoSource = {VideoSource}/> */}
       </div>
     );
   }
--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {Button} from 'mdbreact'
 import ImageItem from './ImageItem';
 import axios from '../axios-db';
+import {storage} from '../App'
+// import firebase from 'firebase';
 
 class ImageList extends Component {
 
@@ -23,7 +25,67 @@ class ImageList extends Component {
             selectedImage: null
         };
     }
+
+    componentDidMount() {
+   //      this.downloadImage();
+    }
     
+
+    downloadImage =() => {
+        var storageRef = storage.ref();
+        // var gsReference = storage.refFromURL ('gs://imagelibrary-47a2f.appspot.com/cola.jpg');
+        storageRef.child('cola.jpg').getDownloadURL().then(function(url) {
+            // `url` is the download URL for 'images/stars.jpg'
+          
+            // This can be downloaded directly:
+            var xhr = new XMLHttpRequest();
+            xhr.responseType = 'blob';
+            xhr.onload = function(event) {
+              var blob = xhr.response;
+            };
+            xhr.open('GET', url);
+            xhr.send();
+          
+            // Or inserted into an <img> element:
+            var img = document.getElementById('myimg');
+            img.src = url;
+          }).catch(function(error) {
+            // Handle any errors
+          });        
+
+
+        // Create a reference to the file we want to download
+        // var pathReference = storage.ref('cola.jpg');
+        // var imageRef = storageRef.child('cola.jpg');
+
+        // // Get the download URL
+        // imageRef.getDownloadURL().then(function(url) {
+        // // Insert url into an <img> tag to "download"
+        //     console.log (url);
+        // }).catch(function(error) {
+
+        // // A full list of error codes is available at
+        // // https://firebase.google.com/docs/storage/web/handle-errors
+        // switch (error.code) {
+        //     case 'storage/object_not_found':
+        //     // File doesn't exist
+        //     break;
+
+        //     case 'storage/unauthorized':
+        //     // User doesn't have permission to access the object
+        //     break;
+
+        //     case 'storage/canceled':
+        //     // User canceled the upload
+        //     break;
+
+        //     case 'storage/unknown':
+        //     // Unknown error occurred, inspect the server response
+        //     break;
+        // }
+        // });
+    }
+
 
     getBase64Image = (props) => {
         var canvas = document.createElement("canvas");
